573638a1dd15d90092dde6d2cf7e1ededb290726
creator god69 <rainbowdeity70@gmail.com>
2020-10-12 00:44:12 +0500

    model user

---
+++ modules/user.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: {
+            count: {
+                type: Number,
+                required: true,
+                default: 1
+            }
+        },
+        serviceId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Service',
+            required: true
+        }
+    }
+})
