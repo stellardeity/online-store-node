edfc232409a53b1e5f81ec6bb2f5ee81b891df2f
creator god69 <rainbowdeity70@gmail.com>
2020-11-28 23:28:24 +0500

    implementation of login

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const User = require("./../modules/user")
 
 const router = Router()
@@ -17,16 +17,53 @@ router.get("/logout", (req, res) => {
 })
 
 router.post("/login", async (req, res) => {
-    const user = await User.findById('5fb57843c45b0b36c43a734e')
-    req.session.user = user
-    req.session.isAuthenticated = true
+    try {
+        const { email, password } = req.body
 
-    req.session.save(err => {
-        if(err) {
-            throw err
+        const candidate = await User.findOne({ email })
+        if (candidate) {
+            const areSame = password === candidate.password
+
+            if (areSame) {
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+
+                req.session.save(err => {
+                    if (err) {
+                        throw err
+                    }
+                    res.redirect("/")
+                })
+            } else {
+                res.redirect("/auth/login#login")
+            }
+        } else {
+            res.redirect("/auth/login#login")
         }
-        res.redirect("/")
-    })
+    } catch (e) {
+        console.log(e)
+    }
+
+})
+
+router.post("/register", async (req, res) => {
+    try {
+        const { email, password, repeat, name } = req.body
+
+        const condidate = await User.findOne({ email })
+
+        if (condidate) {
+            res.redirect("/auth/login#register")
+        } else {
+            const user = new User({
+                email, name, password
+            })
+            await user.save()
+            res.redirect("/auth/login#login")
+        }
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 module.exports = router
